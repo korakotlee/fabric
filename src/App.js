3a26c2c58840571ab14118354c3e96cba9ba4806
Korakot Leemakdej <kleemakdej@gmail.com>
2021-02-13 20:49:17 -0700

    fix: no need to use useState, useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { fabric } from "fabric";
 
 function App() {
+
+  const init = () => {
+    return new fabric.Canvas("c", {
+      height: 500,
+      width: 800,
+      backgroundColor: "green",
+    });
+  }
+  const draw = (canvas) => {
+    const rect = new fabric.Rect({
+      height: 280,
+      width: 200,
+      top: 200,
+      fill: "yellow",
+    });
+    canvas.add(rect);
+    fabric.Image.fromURL(
+      "https://www.google.com/logos/doodles/2021/celebrating-maria-grever-6753651837108287.2-s.png",
+      function (oImg) {
+        // scale image down, and flip it, before adding it onto canvas
+        oImg.scale(3).set("flipX", true);
+        oImg.set("top", 120);
+        oImg.set("left", 20);
+        canvas.add(oImg);
+        canvas.setActiveObject(oImg);
+      }
+    );
+    canvas.renderAll();
+  };
+
+  let canvas = init();
+  draw(canvas);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <canvas id="c"> </canvas>
     </div>
   );
 }
